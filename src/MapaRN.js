8fb36315ccf45f67a4ecc95d42dba79f761478be
Alcides Tiago Medeiros Dantas <alcidesdantasdj@gmail.com>
2025-03-07 10:29:05 -0300

    numbers inside

--- src/MapaRN.js
+++ src/MapaRN.js
@@ -4,7 +4,9 @@ import {
   Geographies,
   Geography,
   ZoomableGroup,
+  Marker,
 } from "react-simple-maps";
+import { geoCentroid } from "d3-geo";
 
 const geoUrl = "/rn.json";
 
@@ -21,21 +23,28 @@ const MapaRN = () => {
       .then((response) => response.json())
       .then((data) => {
         const updatedData = data.features.map((feature) => {
-          const qtdDistribuicao = Math.floor(Math.random() * 100);
+          const distribuicaoMunicipio = Math.floor(Math.random() * 100);
+          const distribuicaoPonto = parseInt(distribuicaoMunicipio / 2);
           const doacaoTotal = Math.floor(Math.random() * 50) + 10;
           const doacaoPessoaFisica = Math.floor(Math.random() * doacaoTotal);
           const doacaoEntidadeBeneficiaria = doacaoTotal - doacaoPessoaFisica;
-          const distribuicao = Math.floor(Math.random() * qtdDistribuicao);
 
           return {
             ...feature,
             properties: {
               ...feature.properties,
-              qtd_distribuicao: qtdDistribuicao,
+              qtd_distribuicao: distribuicaoMunicipio,
               pontos: [
                 {
-                  name: "ponto exemplo",
-                  distribuicao,
+                  name: "Ponto exemplo 1",
+                  distribuicao: distribuicaoPonto,
+                  doacao_total: doacaoTotal,
+                  doacao_pessoa_fisica: doacaoPessoaFisica,
+                  doacao_entidade_beneficiaria: doacaoEntidadeBeneficiaria,
+                },
+                {
+                  name: "Ponto exemplo 2",
+                  distribuicao: distribuicaoPonto,
                   doacao_total: doacaoTotal,
                   doacao_pessoa_fisica: doacaoPessoaFisica,
                   doacao_entidade_beneficiaria: doacaoEntidadeBeneficiaria,
@@ -79,31 +88,43 @@ const MapaRN = () => {
             {({ geographies }) =>
               geographies.map((geo) => {
                 const { name } = geo.properties;
+                const centroid = geoCentroid(geo);
                 return (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    onMouseEnter={(event) => handleMouseEnter(event, name)}
-                    onMouseLeave={() => setHoveredMunicipio(null)}
-                    onClick={() => handleClick(name)}
-                    style={{
-                      default: {
-                        fill:
-                          geo.properties.qtd_distribuicao > 50
-                            ? "#4CAF50"
-                            : "#607D8B",
-                        outline: "none",
-                      },
-                      hover: {
-                        fill: "#FF5722",
-                        outline: "none",
-                      },
-                      pressed: {
-                        fill: "#E64A19",
-                        outline: "none",
-                      },
-                    }}
-                  />
+                  <React.Fragment key={geo.rsmKey}>
+                    <Geography
+                      geography={geo}
+                      onMouseEnter={(event) => handleMouseEnter(event, name)}
+                      onMouseLeave={() => setHoveredMunicipio(null)}
+                      onClick={() => handleClick(name)}
+                      style={{
+                        default: {
+                          fill:
+                            geo.properties.qtd_distribuicao > 80
+                              ? "#4CAF50"
+                              : "#607D8B",
+                          outline: "none",
+                        },
+                        hover: {
+                          fill: "#FF5722",
+                          outline: "none",
+                        },
+                        pressed: {
+                          fill: "#E64A19",
+                          outline: "none",
+                        },
+                      }}
+                    />
+                    <Marker coordinates={centroid}>
+                      <text
+                        textAnchor="middle"
+                        fill="#FFFFFF"
+                        fontSize="2"
+                        fontWeight="bold"
+                      >
+                        {geo.properties.qtd_distribuicao}
+                      </text>
+                    </Marker>
+                  </React.Fragment>
                 );
               })
             }
@@ -117,11 +138,14 @@ const MapaRN = () => {
             position: "absolute",
             top: position.y + 10,
             left: position.x + 10,
-            padding: "5px",
-            background: "rgba(0, 0, 0, 0.7)",
+            padding: "10px",
+            background: "rgba(0, 0, 0, 0.8)",
             color: "white",
-            borderRadius: "3px",
+            borderRadius: "8px",
             pointerEvents: "none",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            fontSize: "16px",
+            lineHeight: "1.5",
           }}
         >
           {hoveredMunicipio.properties.name}
@@ -137,21 +161,38 @@ const MapaRN = () => {
             position: "absolute",
             top: "20px",
             left: "20px",
-            padding: "10px",
-            background: "rgba(0, 0, 0, 0.7)",
+            padding: "20px",
+            background: "rgba(0, 0, 0, 0.8)",
             color: "white",
-            borderRadius: "5px",
+            borderRadius: "10px",
             zIndex: 1000,
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            maxWidth: "300px",
           }}
         >
-          <h3>{selectedMunicipio.properties.name}</h3>
-          <p>
+          <h3 style={{ margin: "0 0 10px 0", fontSize: "18px" }}>
+            {selectedMunicipio.properties.name}
+          </h3>
+          <p style={{ margin: "0 0 10px 0", fontSize: "14px" }}>
+            Quantidade de distribuição:{" "}
+            {selectedMunicipio.properties.qtd_distribuicao}
+          </p>
+          <p style={{ margin: "0 0 10px 0", fontSize: "14px" }}>
             Informações adicionais sobre {selectedMunicipio.properties.name}
           </p>
-          <p>Pontos:</p>
+          <p style={{ margin: "0 0 10px 0", fontSize: "14px" }}>Pontos:</p>
           {selectedMunicipio.properties.pontos.map((ponto) => (
-            <div key={ponto.name}>
+            <div key={ponto.name} style={{ marginBottom: "10px" }}>
               <button
+                style={{
+                  padding: "8px 12px",
+                  background: "#4CAF50",
+                  color: "white",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  fontSize: "14px",
+                }}
                 onClick={() =>
                   setSelectedMunicipio({
                     ...selectedMunicipio,
@@ -161,34 +202,37 @@ const MapaRN = () => {
               >
                 {ponto.name} - Distribuição: {ponto.distribuicao}
               </button>
-              <br />
             </div>
           ))}
           {selectedMunicipio.selectedPonto && (
             <div
               style={{
                 position: "absolute",
-                top: "200px",
+                top: "250px",
                 left: "20px",
-                padding: "10px",
-                background: "rgba(0, 0, 0, 0.7)",
+                padding: "20px",
+                background: "rgba(0, 0, 0, 0.8)",
                 color: "white",
-                borderRadius: "5px",
+                borderRadius: "10px",
                 zIndex: 999,
+                boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+                maxWidth: "300px",
               }}
             >
-              <h4>{selectedMunicipio.selectedPonto.name}</h4>
-              <p>
+              <h4 style={{ margin: "0 0 10px 0", fontSize: "16px" }}>
+                {selectedMunicipio.selectedPonto.name}
+              </h4>
+              <p style={{ margin: "0 0 8px 0", fontSize: "14px" }}>
                 Distribuição: {selectedMunicipio.selectedPonto.distribuicao}
               </p>
-              <p>
+              <p style={{ margin: "0 0 8px 0", fontSize: "14px" }}>
                 Doação total: {selectedMunicipio.selectedPonto.doacao_total}
               </p>
-              <p>
+              <p style={{ margin: "0 0 8px 0", fontSize: "14px" }}>
                 Doação pessoa física:{" "}
                 {selectedMunicipio.selectedPonto.doacao_pessoa_fisica}
               </p>
-              <p>
+              <p style={{ margin: "0 0 8px 0", fontSize: "14px" }}>
                 Doação pessoa entidade:{" "}
                 {selectedMunicipio.selectedPonto.doacao_entidade_beneficiaria}
               </p>
